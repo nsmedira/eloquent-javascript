0e16718c86be49ce47218f0bbdfd20f8b7354d1f
Nick Smedira <nsmedira@me.com>
2020-12-25 18:54:45 -0500

    chapter 2 exercises

---
+++ chapter-02/fizzbuzz-1.js
@@ -0,0 +1,9 @@
+for ( let i = 1 ; i <= 100 ; i++ ) {
+    if ( !(i % 3) ) {
+        console.log('fizz')
+    } else if ( !(i % 5)) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+}
---
+++ chapter-02/fizzbuzz-2.js
@@ -0,0 +1,17 @@
+for ( let i = 1 ; i <= 100 ; i++ ) {
+
+    if ( !(i % 3) && !(i % 5) ) {
+        console.log('fizzbuzz')
+
+    } else if ( !(i % 3) ) {
+        console.log('fizz')
+
+    } else if ( !(i % 5)) {
+        console.log('buzz')
+
+    } else {
+        console.log(i)
+
+    }
+    
+}
