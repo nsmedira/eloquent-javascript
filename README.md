a849898c5ec71d3d3e645d1fe2065715b27ae721
Nick Smedira <nsmedira@me.com>
2022-01-31 06:56:03 -0500

    add code sandbox link to readme exercise: A Vector Type

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eloquent-javascript
+
 Exercises and notes from Eloquent Javascript by Marijn Haverbeke
+
+[Code Sandbox and Exercise Solutions](https://eloquentjavascript.net/code/)
---
+++ chapter-06-objects/a-vector-type.js
@@ -0,0 +1,25 @@
+// write a class Vec that represents a vector in two-dimensional space
+// it takes x and y parameters (numbers), which it should save to properties of the same name
+
+// give the Vec prototype two methods: plus and minus
+// that takes another vector as a parameter and returns a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values
+
+// add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0)
+
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus = (vector) => new Vec(this.x + vector.x, this.y + vector.y);
+  minus = (vector) => new Vec(this.x - vector.x, this.y - vector.y);
+
+  get length() {
+    return Math.sqrt((this.x - 0) ** 2 + (this.y - 0) ** 2);
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3))); // → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3))); // → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length); // → 5
